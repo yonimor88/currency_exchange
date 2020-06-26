4875c19cd0ad6a125302c4ce8c8accd0d311d09a
yonimor88 <yonimor88@gmail.com>
2020-06-26 11:30:38 +0300

    1

--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -1,48 +1,53 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function Exchange() {
+    const [currency, setCurrency]= useState([{type:'dollar', value:4},{type:'euro', value:3},{type:'shekel', value:1}])
+    const [firstSelect, setFirstSelect]=useState('')
+    const [secondSelect, setSecondSelect]=useState('')
+    const [result, setResult]=useState(0)
+    const [input, setInput]= useState(null)
 
-    const [firstType, setFirstType]= useState([])
-    const [secondType, setSecondType]= useState([])
+  const firstRoomType = (e)=>{
+    setFirstSelect(e.target.value);
+  }
 
-  return (
+  const secondRoomType = (e)=>{
+    setSecondSelect(e.target.value)}
+
+  
+  const start = () =>{
+    
+  }
+
+    return (
     <div className="Exchange">
       From: <select
         name="type"
         id="type"
-        onChange={(e) => {
-          setFirstType(e.target.value);
-        }}
-      >
-        <option hidden disabled selected value>
-          Type{" "}
-        </option>
-        <option value="Dollar">Dollar</option>
-        <option value="Euro">Euro</option>
-        <option value="Shekel">Shekel</option>
+        onChange={firstRoomType}>
+        <option hidden disabled selected value>Type{" "}</option>
+        {
+        currency.map(item =><option value={item.type} key={item.type}>{item.type}</option>)
+        }
       </select>
-      <input placeholder="Enter Amount"></input>
+      <input placeholder="Enter Amount" onChange={(e)=>setInput(e.target.value)}></input>
       <br />
       To :<select
         name="type"
         id="type"
-        onChange={(e) => {
-          setSecondType(e.target.value);
-        }}
-      >
-        <option hidden disabled selected value>
-          Type{" "}
-        </option>
-        <option value="Dollar">Dollar</option>
-        <option value="Euro">Euro</option>
-        <option value="Shekel">Shekel</option>
+        onChange={secondRoomType}>
+        <option hidden disabled selected value>Type{" "}</option>
+        {
+        currency.map(item =><option value={item.type} key={item.type}>{item.type}</option>)
+        }
       </select>
       <br />
-      <button style={{backgroundColor:'lightgray', alignSelf:'right'}}>Start</button>
+      <button style={{backgroundColor:'lightgray', alignSelf:'right'}} onClick={start}>Start</button>
       <br />
       <Link to="/Update">
-        <button className="update_btn">Update</button>
+        <button className="update_btn" >Update</button>
       </Link>
       <a href="https://www.facebook.com" className="facebook_btn">
         <button>Facebook</button>
